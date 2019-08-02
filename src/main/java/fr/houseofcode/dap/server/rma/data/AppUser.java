d3028d0fca729f230a2ee38cc993277c0b8c46b3
Kelly Gauthier <laviolettek@gmail.com>
2019-08-02 16:52:53 +0200

    HelloWorld jpa

---
+++ src/main/java/fr/houseofcode/dap/server/rma/data/AppUser.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package fr.houseofcode.dap.server.rma.data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+/**
+ * @author lavio
+ *
+ */
+@Entity
+public class AppUser {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    private String name;
+    
+
+    
+}
